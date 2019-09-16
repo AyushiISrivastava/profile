f542d792a3541a79eff99935dfb42bc2c2a0cb0b
Ayushi Srivastava <ayushis2k10@gmail.com>
2019-09-16 22:14:01 +0530

    Bug fixes

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ mongoose.connect('mongodb://localhost:27017/profile',
 
 console.log(__dirname);
 
+// app.options('*', cors());
+
 app.use(cors());
 
 app.get('/',function(req,res){
--- profile.js
+++ profile.js
@@ -1,120 +1,116 @@
-var times = 0;
 var cookie_name = "Counter_Cookie";
 document.getElementById("button").addEventListener("click", function(event){
 	event.preventDefault();
-	if(times > 0){
+	if(document.getElementById("username").value == ''){
 		var snackBar = document.getElementById("snackbar");
    	    snackBar.className = "show";
-	    snackBar.innerHTML = "Already submitted";
+	    snackBar.innerHTML = "Please enter name";
 	    snackbar.style.background = "#FF4C4C";
   		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
   		return;
-	} else{
-		if(document.getElementById("username").value == ''){
-			var snackBar = document.getElementById("snackbar");
-	   	    snackBar.className = "show";
-		    snackBar.innerHTML = "Please enter name";
-		    snackbar.style.background = "#FF4C4C";
-	  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
-	  		return;
-		}
-		if(document.getElementById("phno").value == ''){
-			var snackBar = document.getElementById("snackbar");
-	   	    snackBar.className = "show";
-		    snackBar.innerHTML = "Please enter phone number";
-		    snackbar.style.background = "#FF4C4C";
-	  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
-	  		return;
-		} else if(document.getElementById("phno").value.length < 10 || document.getElementById("phno").value.length > 10){
-			var snackBar = document.getElementById("snackbar");
-	   	    snackBar.className = "show";
-		    snackBar.innerHTML = "Please enter a valid phone number";
-		    snackbar.style.background = "#FF4C4C";	    
-	  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
-	  		return;
-		} 
-		if(document.getElementById("email").value == ''){
-			var snackBar = document.getElementById("snackbar");
-	   	    snackBar.className = "show";
-		    snackBar.innerHTML = "Please enter email id";
-		    snackbar.style.background = "#FF4C4C";
-	  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
-	  		return;
-		} else {
-		    var regularExpression = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		    if(!regularExpression.test(document.getElementById("email").value)){
-				var snackBar = document.getElementById("snackbar");
-		   	    snackBar.className = "show";
-			    snackBar.innerHTML = "Please enter a valid email id";
-			    snackbar.style.background = "#FF4C4C";
-		  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
-		  		return;
-	  		}
-		}
-		if(document.getElementById("title").value == ''){
-			var snackBar = document.getElementById("snackbar");
-	   	    snackBar.className = "show";
-		    snackBar.innerHTML = "Please enter job title";
-		    snackbar.style.background = "#FF4C4C";
-	  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
-	  		return;
-		}
-		if(document.getElementById("resume").files.length == 0){
-			var snackBar = document.getElementById("snackbar");
-	   	    snackBar.className = "show";
-		    snackBar.innerHTML = "Please select resume";
-		    snackbar.style.background = "#FF4C4C";
-	  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
-	  		return;
-		}
-		const url = "http://localhost:3000/api/save";
-	    const data = {
-	        'resumeUploaded' : document.getElementById("resume").files[0],
-	        'username': document.getElementById("username").value,
-	        'phoneNo' : document.getElementById("phno").value,
-	        'email' : document.getElementById("email").value,
-	        'jobTitle' : document.getElementById("title").value
-	    };
-		axios({
-		    method: 'post',
-		    url: url,
-		    data: data,
-		    config: { headers: { 'crossDomain': true,
-									'Content-Type': 'multipart/form-data'}}
-		    })
-		    .then(function (response) {
-		    	if(response.data == "INVALID_EMAIL") {
-		    		var snackBar = document.getElementById("snackbar");
-			   	    snackBar.className = "show";
-				    snackBar.innerHTML = "Invalid Email";
-				    snackbar.style.background = "#FF4C4C";
-			  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
-			  		return;
-		    	} else if (response.data == "INVALID_PHNO"){
-		    		var snackBar = document.getElementById("snackbar");
-			   	    snackBar.className = "show";
-				    snackBar.innerHTML = "Invalid Phone Number";
-				    snackbar.style.background = "#FF4C4C";
-			  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
-			  		return;
-		    	} else {
-				    var snackBar = document.getElementById("snackbar");
-			   	    snackBar.className = "show";
-				    snackBar.innerHTML = "Success!";
-				    snackbar.style.background = "#00CC00";
-			  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
-		        	times++;	
-	        	}
-		    })
-		    .catch(function (response) {
-				var snackBar = document.getElementById("snackbar");
-		   	    snackBar.className = "show";
-			    snackBar.innerHTML = response.data;
-			    snackbar.style.background = "#FF4C4C";
-		  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
-		  		return;
-		});
 	}
+	if(document.getElementById("phno").value == ''){
+		var snackBar = document.getElementById("snackbar");
+   	    snackBar.className = "show";
+	    snackBar.innerHTML = "Please enter phone number";
+	    snackbar.style.background = "#FF4C4C";
+  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
+  		return;
+	} else if(document.getElementById("phno").value.length < 10 || document.getElementById("phno").value.length > 10){
+		var snackBar = document.getElementById("snackbar");
+   	    snackBar.className = "show";
+	    snackBar.innerHTML = "Please enter a valid phone number";
+	    snackbar.style.background = "#FF4C4C";	    
+  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
+  		return;
+	} 
+	if(document.getElementById("email").value == ''){
+		var snackBar = document.getElementById("snackbar");
+   	    snackBar.className = "show";
+	    snackBar.innerHTML = "Please enter email id";
+	    snackbar.style.background = "#FF4C4C";
+  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
+  		return;
+	} else {
+	    var regularExpression = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	    if(!regularExpression.test(document.getElementById("email").value)){
+			var snackBar = document.getElementById("snackbar");
+	   	    snackBar.className = "show";
+		    snackBar.innerHTML = "Please enter a valid email id";
+		    snackbar.style.background = "#FF4C4C";
+	  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
+	  		return;
+  		}
+	}
+	if(document.getElementById("title").value == ''){
+		var snackBar = document.getElementById("snackbar");
+   	    snackBar.className = "show";
+	    snackBar.innerHTML = "Please enter job title";
+	    snackbar.style.background = "#FF4C4C";
+  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
+  		return;
+	}
+	if(document.getElementById("resume").files.length == 0){
+		var snackBar = document.getElementById("snackbar");
+   	    snackBar.className = "show";
+	    snackBar.innerHTML = "Please select resume";
+	    snackbar.style.background = "#FF4C4C";
+  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
+  		return;
+	}
+	const url = "/api/save";
+    const data = {
+        'resumeUploaded' : document.getElementById("resume").files[0],
+        'username': document.getElementById("username").value,
+        'phoneNo' : document.getElementById("phno").value,
+        'email' : document.getElementById("email").value,
+        'jobTitle' : document.getElementById("title").value
+    };
+	axios({
+	    method: 'post',
+	    url: url,
+	    data: data,
+	    config: { headers: { 'crossDomain': true,
+								'Content-Type': 'multipart/form-data'}}
+	    })
+	    .then(function (response) {
+	    	if(response.data == "INVALID_EMAIL") {
+	    		var snackBar = document.getElementById("snackbar");
+		   	    snackBar.className = "show";
+			    snackBar.innerHTML = "Invalid Email";
+			    snackbar.style.background = "#FF4C4C";
+		  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
+		  		return;
+	    	} else if (response.data == "INVALID_PHNO"){
+	    		var snackBar = document.getElementById("snackbar");
+		   	    snackBar.className = "show";
+			    snackBar.innerHTML = "Invalid Phone Number";
+			    snackbar.style.background = "#FF4C4C";
+		  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
+		  		return;
+	    	} else {
+			    var snackBar = document.getElementById("snackbar");
+		   	    snackBar.className = "show";
+			    snackBar.innerHTML = "Success!";
+			    snackbar.style.background = "#00CC00";
+		  		setTimeout(function(){ 
+		  			snackBar.className = snackBar.className.replace("show", "");
+		  			document.getElementById("username").value = '';
+		  			document.getElementById("phno").value = '';
+		  			document.getElementById("email").value = '';
+		  			document.getElementById("title").value = '';
+		  			document.getElementById("resume").value = '';
+		  		 }, 3000);
+        	}
+	    })
+	    .catch(function (response) {
+			var snackBar = document.getElementById("snackbar");
+	   	    snackBar.className = "show";
+		    snackBar.innerHTML = response.data;
+		    snackbar.style.background = "#FF4C4C";
+	  		setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
+	  		return;
+	});
 });
 
 function preventInput(evnt) {
